7c6ce815a77cdf4ad8167a8ac93aa5fdab429458
gg <harshit.ixora@gmail.com>
2021-10-03 07:32:29 +0530

    Commited By Harshit

--- app/src/main/java/com/my/localizadorapp/act/FaqScreen.java
+++ app/src/main/java/com/my/localizadorapp/act/FaqScreen.java
@@ -8,6 +8,7 @@ import com.my.localizadorapp.R;
 
 public class FaqScreen extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
